1b24c71d92f1b8f83a935f4fe7d0ac1d1e463d8d
Raphael Pyo <62922500+raphaelmhpyo@users.noreply.github.com>
2021-11-16 09:33:29 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
-function incomeDistribution(){
-  var income = document.querySelector("input#income").value;
+function paedCalculation() {
+  // Declare variables for entered inputs
+  var age = Number($("#ageEntered").val());
+  var wtEntered = Number($("#weightEntered").val());
+
+  // Declare calculated variables
+  var wtCalc = (age + 4) * 2;
+  if (wtEntered === 0) {
+    var weight = wtCalc;
+  } else {
+    weight = wtEntered;
+  }
+  var sbp = 80 + age * 2;
+  var shock = weight * 4;
+  var atropine = weight * 20;
+  var adrenaline = weight * 10;
+  var amiodarone = weight * 5;
+  var fent = weight * 2;
+  var prop = weight * 4;
+  var sux = weight * 2;
+  var ondans = (weight * 0.15).toFixed(1);
+  var dexa = (weight * 0.15).toFixed(1);
+  var paracetamol = weight * 15;
+  var ibuprofen = weight * 10;
+  var ketorolac = weight * 2;
+
+  var ett = (age / 4) + 4;
+  var depth = age / 2 + 12;
+
+  // LMA sizing algorithm
+  if (weight < 5) {
+    var lma = 1;
+  } else if (weight >= 5 && weight < 10) {
+    lma = 1.5;
+  } else if (weight >= 10 && weight < 20) {
+    lma = 2;
+  } else if (weight >= 20 && weight < 30) {
+    lma = 2.5;
+  } else if (weight >= 30) {
+    lma = 3;
+  }
+
+  var paedAnaesCategories = "Weight (kg):\nsBP (mmHg):\n\nShock (J):\nAmiodarone (mg):\nAdrenaline (mcg):\nAtropine (mcg):\n\nETT size:\nETT depth (cm):\nLMA size:\n\nFentanyl (mcg):\nPropofol (mg):\nSuxamethonium (mg):\n\nOndansetron (mg):\nDexamethasone (mg):\nParacetamol (mg):\nIbuprofen (mg):\nKetorolac (mg):";
+  var paedAnaesOutput = weight + "\n" + sbp + "\n\n" + shock + "\n" + amiodarone + "\n" + adrenaline + "\n" + atropine + "\n\n" + ett + "\n" + depth + "\n" + lma + "\n\n" + fent + "\n" + prop + "\n" + sux + "\n\n" + ondans + "\n" + dexa + "\n" + paracetamol + "\n" + ibuprofen + "\n" + ketorolac;
+  document.querySelector("#paedCalculatorOutputCategories").innerText = paedAnaesCategories;
+  document.querySelector("#paedCalculatorOutput").innerText = paedAnaesOutput;
+}
+
+
+
+function incomeDistribution() {
+  var income = Number($("#income").val());
+  // var income = Number(document.querySelector("#income").value);
   var mojo = 0;
   var parents = 0;
   var shares = 0;
 
-  if (document.querySelector("#mojo").checked === true){
-    if (income > 100){
-      income-=100;
+  if ($("#mojo").is(":checked")) {
+    if (income > 100) {
+      income -= 100;
       mojo = 100;
     } else {
-      alert("Insufficient funds!");
+      alert("Insufficient funds - Mojo");
     }
   }
 
-  if (document.querySelector("#parents").checked === true){
-    if (income > 800){
-      income-=800;
+  if ($("#parents").is(":checked")) {
+    if (income > 800) {
+      income -= 800;
       parents = 400;
     } else {
-      alert("Insufficient funds!");
+      alert("Insufficient funds - Parents");
     }
   }
 
-  if (document.querySelector("#shares").checked === true){
-    if (income > 1000){
-      income-=1000;
+  if ($("#shares").is(":checked")) {
+    if (income > 1000) {
+      income -= 1000;
       shares = 1000;
+    } else {
+      alert("Insufficient funds - Shares");
     }
-  } else {
-    alert("Insufficient funds!");
   }
 
   var fe = income * 0.65;
