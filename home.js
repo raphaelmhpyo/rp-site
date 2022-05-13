201294e2d8639c0f988a43e4ed2ff96c71d385a6
Raphael Pyo <62922500+raphaelmhpyo@users.noreply.github.com>
2022-05-14 02:54:14 +1000

    Update home.js
    updated password

--- home.js
+++ home.js
@@ -1,5 +1,11 @@
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+passwd = dd + mm + yyyy + "pAssworD";
+
 var pw = prompt("Enter calculation input:");
-if (pw === "pAssworD"){
+if (pw === passwd){
 } else {
   window.location.href = "index.html";
 }
